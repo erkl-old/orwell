f8e2a4e696bf970ea6cad57cac477c2de54495c1
Erik Lundin <erklundin@gmail.com>
2013-11-01 14:18:24 +0000

    Implement the ow__chain utility struct

--- src/orwell-util.c
+++ src/orwell-util.c
@@ -24,3 +24,50 @@ int ow__readln(FILE *file, char *buf, size_t len) {
 
     return 0;
 }
+
+/*
+ * Appends a copy of s to the end of the chain.
+ */
+int ow__chain_add(struct ow__chain *ch, const char *s) {
+    size_t index = 0;
+    size_t limit = ch->boundary - ch->tail;
+    unsigned char c;
+
+    do {
+        if (index >= limit) {
+            return EOVERFLOW;
+        }
+
+        ch->tail[index] = (c = s[index]);
+        index++;
+    } while (c != '\0');
+
+    ch->tail += index;
+    return 0;
+}
+
+/*
+ * Searches the chain for needle, returning the position of the
+ * matching entry if found.
+ */
+char *ow__chain_find(struct ow__chain *ch, const char *needle) {
+    char *curr = ch->head;
+    size_t i;
+
+    while (curr < ch->tail) {
+        for (i = 0; ; i++) {
+            if (curr[i] != needle[i]) {
+                while (curr[i++] != '\0');
+                break;
+            }
+
+            if (needle[i] == '\0') {
+                return curr;
+            }
+        }
+
+        curr += i;
+    }
+
+    return NULL;
+}
--- src/orwell-util.h
+++ src/orwell-util.h
@@ -8,3 +8,18 @@
  * if the operation failed.
  */
 int ow__readln(FILE *file, char *buf, size_t len);
+
+/*
+ * A chain is a bounded sequence of NULL-terminated strings. It allows
+ * us to store an array of strings inside a user-provided buffers.
+ */
+struct ow__chain {
+    char *head;
+    char *tail;
+    char *boundary;
+};
+
+int ow__chain_add(struct ow__chain *chain, const char *s);
+char *ow__chain_find(struct ow__chain *chain, const char *needle);
+
+#endif
